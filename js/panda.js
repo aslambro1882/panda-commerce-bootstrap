c60b1800ca02391c08e199b63aa2df618af909a1
aslambro1882 <0909maruf@gmail.com>
2021-08-14 09:31:25 +0600

    adding js

---
+++ js/panda.js
@@ -0,0 +1,67 @@
+const h2 = document.getElementsByTagName('h2');
+for (const h of h2) {
+    h.style.color = 'lightblue';
+}
+
+
+
+const backpack = document.getElementById('backpack');
+backpack.style.backgroundColor = 'tomato';
+backpack.style.padding = '10px';
+
+
+
+const cards = document.getElementsByClassName('card');
+for (const card of cards) {
+    card.style.borderRadius = '30px';
+}
+
+
+
+function clicking() {
+    console.log('click');
+}
+
+
+
+document.getElementsByClassName('buy-now')
+const buyButtons = document.getElementsByClassName('buy-now');
+for (const buyButton of buyButtons) {
+    buyButton.addEventListener('click', function () {
+        buyButton.style.display = 'none';
+    })
+}
+
+
+
+const submit = document.getElementById('submit');
+submit.setAttribute('disabled', true);
+const inputvalue = document.getElementById('input-area')
+inputvalue.addEventListener('keyup', function (event) {
+    if (event.target.value == 'email') {
+        submit.removeAttribute('disabled');
+    }
+    else {
+        submit.setAttribute('disabled', true);
+    }
+})
+
+
+
+const changeImage = document.getElementById('change-image');
+changeImage.addEventListener('mouseenter', function () {
+    changeImage.src = 'images/shoes/shoe-2.png';
+})
+changeImage.addEventListener('mouseleave', function () {
+    changeImage.src = 'images/shoes/shoe-1.png';
+    console.log('image found');
+})
+
+
+
+const touchArea = document.getElementById('touch-area');
+touchArea.addEventListener('dblclick', function (event) {
+    event.target.style.backgroundColor = 'green';
+    event.stopImmediatePropagation;
+})
+
